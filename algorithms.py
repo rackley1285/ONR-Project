875f953bd7ab5852205c4a2f3803eacb3a84d924
rackley1285 <140672682+rackley1285@users.noreply.github.com>
2026-02-12 18:01:46 +0000

    Correct MD_ordering and degeneracy functions

--- algorithms.py
+++ algorithms.py
@@ -1,30 +1,51 @@
 import networkx as nx
 
 def MD_ordering(graph):
+    MD_list = []
     j = 0
     H = graph.copy()
-    deg_list = sorted(list(graph.degree), key=lambda node: node[1])
-    MD_list = []
+    deg_list = sorted(list(graph.degree), key=lambda x: x[1])
     while j < len(graph.nodes):
         MD_list.append(deg_list[j][0])
         H = H.subgraph(v for v in list(graph) if v not in MD_list)
         j += 1
-
+    
     return MD_list
 
-def degeneracy(graph, MD = None):
-    if MD == None:
-        MD = MD_ordering(graph)
+
+def degeneracy(graph):
+    L = set()
+    deg_list = dict(graph.degree)
+    max_deg = max(deg_list.values())
+
+    D = [[] for i in range(max_deg + 1)]  #Buckets for possible vertex degrees
+    for v, deg in deg_list.items():  #Filling buckets
+        D[deg].append(v)
+    d = 0  #Degeneracy of the graph
     
-    D = []
-    for v in MD:
-        Nv = list(graph.neighbors(v))
-        rnbrhd = [n for n in MD[MD.index(v):] if n in Nv]
-        D.append(len(rnbrhd))
-    
-    return max(D)
+    for x in graph.nodes:
+        i = 0
+        # Scan buckets until a nonempty bucket is found
+        while i < max_deg and len(D[i]) == 0:
+            i += 1
+        v = D[i].pop()
+        L.add(v)
+        d = i if i > d else d #Update degeneracy with degree of found vertex
+            
+        
+        #Update buckets for neighbors of v
+        for u in graph.neighbors(v):
+            if u in L:
+                continue
+            for i in range(len(D)):
+                if u in D[i]:
+                    D[i].remove(u)
+                    D[i-1].append(u)
+                    break
+        
+    return d
     
 
 G = nx.path_graph(4)
-print(degeneracy(G))
+print(MD_ordering(G), degeneracy(G))
 
