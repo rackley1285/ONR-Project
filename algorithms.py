2848d2230e67ee53ce3736bd1b683da5cce76554
rackley1285 <140672682+rackley1285@users.noreply.github.com>
2026-02-16 15:56:45 +0000

    Add function to find a random clique of size q

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,5 @@
 import networkx as nx
-
+import igraph as ig
 
 # Create a minimum-degree ordered list
 #--------------------------------------------------------------------------------
@@ -74,10 +74,74 @@ def WB_max_clq(graph, p):
 #--------------------------------------------------------------------------------
 
 
+# Find random clique of size q
+#--------------------------------------------------------------------------------
+def find_clique(graph, q):
+    v1 = 0
+    for i in range(graph.vcount()):
+        if graph.degree(i) == q:
+            v1 = i
+            break
+    
+    C = [v1]
+    C_nbrs = [(v1, neighbor) for neighbor in graph.neighborhood(v1)]
+    for v1, neighbor in C_nbrs:
+        if graph.degree(neighbor) >= q and neighbor not in C:
+            C.append(neighbor)
+    
+    i = 1
+    while len(C) > q and i < len(C):
+        for j in range(1, len(C)):
+            if not graph.are_adjacent(C[i], C[j]) and C[i] != C[j]:
+                C.pop(j)
+        i += 1
+    
+    if len(C) < q:
+        return []
+    else:
+        return sorted(C)
+#--------------------------------------------------------------------------------
+
+
+# Read in DIMACS10 file format
+#--------------------------------------------------------------------------------
+def rd(pathname,filename):
+    """Read graph instance in DIMACS clustering challenge format."""
+    print("DIMACS10 Instance:",filename)
+    with open(pathname+filename,'r') as infile:
+        line = infile.readline()
+        vertices = int(line.split()[0])
+        edges = int(line.split()[1])
+        fmt = int(line.split()[2])
+        G = ig.Graph()
+        if fmt==0:
+            print("#Vertices",vertices)
+            print("#Edges",edges)
+            for i in range(vertices):
+                G.add_vertex(name = str(i+1))
+            
+            u = 0
+            while (u <= vertices):
+                line = infile.readline()
+                if (not line.strip()):
+                    u = u + 1
+#                    print("Vertex", u, " is isolated.")
+                elif (line[0] == '%'): 
+                    print("Skipping comment: ",line)
+                else:
+                    u = u + 1
+                    for word in line.split():
+                        G.add_edge(u-1,(int(word)-1))
+        else:
+            print("DIMACS10 weighted graphs need a new reader!")
+    return G
+#--------------------------------------------------------------------------------
+
+
 # Testing grounds
 #--------------------------------------------------------------------------------
 if __name__ == "__main__":
-    G = nx.path_graph(4)
-    print(MD_ordering(G), degeneracy(G))
-    WB_max_clq(G, 0)
+    G = rd("/workspaces/ONR-Project/testbed/", "CIP_example.graph").simplify()
+    print(find_clique(G, 4))
+    #WB_max_clq(G, 0)
 
