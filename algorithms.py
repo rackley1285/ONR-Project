3f6bf0c5fecf75a3e2eb510d25e3f5c0311cc448
Kaden Rackley <rackley1285@gmail.com>
2026-02-16 15:39:59 -0600

    Remove unnecessary functions and improve rd() reading speed

--- algorithms.py
+++ algorithms.py
@@ -2,78 +2,6 @@ import networkx as nx
 import igraph as ig
 import sys, time
 
-# Create a minimum-degree ordered list
-#--------------------------------------------------------------------------------
-def MD_ordering(graph):
-    MD_list = []
-    j = 0
-    H = graph.copy()
-    deg_list = sorted(list(graph.degree), key=lambda x: x[1])
-    while j < len(graph.nodes):
-        MD_list.append(deg_list[j][0])
-        H = H.subgraph(v for v in list(graph) if v not in MD_list)
-        j += 1
-    
-    return MD_list
-#--------------------------------------------------------------------------------
-
-
-# Calculate the degeneracy of a graph
-#--------------------------------------------------------------------------------
-def degeneracy(graph):
-    L = set()
-    deg_list = dict(graph.degree)
-    max_deg = max(deg_list.values())
-
-    D = [[] for i in range(max_deg + 1)]  #Buckets for possible vertex degrees
-    for v, deg in deg_list.items():  #Filling buckets
-        D[deg].append(v)
-    d = 0  #Degeneracy of the graph
-    
-    for x in graph.nodes:
-        i = 0
-        # Scan buckets until a nonempty bucket is found
-        while i < max_deg and len(D[i]) == 0:
-            i += 1
-        v = D[i].pop()
-        L.add(v)
-        d = i if i > d else d #Update degeneracy with degree of found vertex
-            
-        
-        #Update buckets for neighbors of v
-        for u in graph.neighbors(v):
-            if u in L:
-                continue
-            u_deg = deg_list[u]
-            deg_list[u] -= 1
-            D[u_deg].remove(u)
-            D[u_deg - 1].append(u)
-        
-    return d
-#--------------------------------------------------------------------------------
-
-# Walteros-Buchanan maximum clique algorithm
-#--------------------------------------------------------------------------------
-def WB_max_clq(graph, p):
-    V = MD_ordering(graph)
-    indices = range(len(V))
-    d = degeneracy(graph)
-    n = len(V)
-
-    D = []
-    D_rns = []
-    for i in indices:
-        r_neighborhood = [V[j] for j in indices[i:] if graph.has_edge(V[i], V[j])]
-        rdeg = len(r_neighborhood)
-        if i < n - d and rdeg >= d - p:
-            D.append(V[i])
-            D_rns.append(r_neighborhood)
-    
-    for v_rn in D_rns:
-        G_bar = graph.subgraph()
-
-#--------------------------------------------------------------------------------
-
 
 # Read in DIMACS10 file format
 #--------------------------------------------------------------------------------
@@ -90,8 +18,7 @@ def rd(pathname,filename):
         if fmt==0:
             print("#Vertices",vertices)
             print("#Edges",edges)
-            for i in range(vertices):
-                G.add_vertex(name = str(i+1))
+            G.add_vertices(range(vertices))
             
             u = 0
             edges = []
